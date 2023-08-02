4d58afe0a85816d60d18c6c6d581ccb30eb356c4
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2023-08-02 18:35:47 +0500

    function sub unit tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -12,3 +12,14 @@ class AddTest(unittest.TestCase):
 
     def test_add_1000_and_1000(self):
         self.assertEqual(calculator.add(1000, 1000), 2000)
+
+
+class AddSub(unittest.TestCase):
+    def test_sub_1_and_1(self):
+        self.assertEqual(calculator.sub(1, 1), 0)
+
+    def test_sub_10_and_1(self):
+        self.assertEqual(calculator.sub(10, 1), 9)
+
+    def test_sub_1000_and_5000(self):
+        self.assertEqual(calculator.sub(1000, 5000), -4000)
